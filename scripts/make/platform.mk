1a339514bda303fb45d29dfeca11045e2d8de6b0
Youjie Zheng <Azure_stars@126.com>
2025-06-18 13:53:31 +0800

    [build] simplify the process of determining the configuration platform

--- scripts/make/platform.mk
+++ scripts/make/platform.mk
@@ -1,67 +1,48 @@
 # Architecture and platform resolving
 
-# Builtin platforms in [axhal_crates](https://github.com/arceos-org/axplat_crates/tree/main/platforms)
-builtin_platforms := x86_64-qemu-q35 \
-    riscv64-qemu-virt \
-    aarch64-qemu-virt \
-    aarch64-raspi4 \
-    aarch64-bsta1000b \
-    aarch64-phytium-pi \
-    loongarch64-qemu-virt
-
-# Resolve the path of platform configuration file
-define resolve_plat_config
-  $(shell cargo axplat info $(PLAT_PACKAGE)  | grep '^config_path:' | cut -d ' ' -f2-)
-endef
 ifeq ($(MYPLAT),)
   # `PLATFORM` is not specified, use the default platform for each architecture
   ifeq ($(ARCH), x86_64)
-    PLAT_NAME := x86_64-qemu-q35
     PLAT_PACKAGE := axplat-x86-pc
   else ifeq ($(ARCH), aarch64)
-    PLAT_NAME := aarch64-qemu-virt
     PLAT_PACKAGE := axplat-aarch64-qemu-virt
   else ifeq ($(ARCH), riscv64)
-    PLAT_NAME := riscv64-qemu-virt
     PLAT_PACKAGE := axplat-riscv64-qemu-virt
   else ifeq ($(ARCH), loongarch64)
-    PLAT_NAME := loongarch64-qemu-virt
     PLAT_PACKAGE := axplat-loongarch64-qemu-virt
   else
     $(error "ARCH" must be one of "x86_64", "riscv64", "aarch64" or "loongarch64")
   endif
-  PLAT_CONFIG := $(call resolve_plat_config)
+  PLAT_CONFIG := $(shell cargo axplat info -c $(PLAT_PACKAGE) 2>/dev/null)
 else
   # `PLATFORM` is specified, override the `ARCH` variables
-  ifneq ($(filter $(MYPLAT), $(builtin_platforms)),)
-    # builtin platform
-    ifeq ($(MYPLAT), x86_64-qemu-q35)
-      PLAT_PACKAGE := axplat-x86-pc
-    else ifeq ($(MYPLAT), aarch64-raspi4)
+  ifneq ($(wildcard $(MYPLAT)),)
+    # custom platform, read the "plat-name" fields from the toml file
+    PLAT_CONFIG := $(MYPLAT)
+    PLAT_PACKAGE := $(patsubst "%",%,$(shell axconfig-gen $(PLAT_CONFIG) -r package))
+  else 
+    # treat `MYPLAT` as a pagckage name
+    ifeq ($(MYPLAT), aarch64-raspi4)
       PLAT_PACKAGE := axplat-aarch64-raspi
     else
       PLAT_PACKAGE := axplat-$(MYPLAT)
     endif
-    PLAT_NAME := $(MYPLAT)
-    PLAT_CONFIG := $(call resolve_plat_config)
-    _arch := $(patsubst "%",%,$(shell axconfig-gen $(PLAT_CONFIG) -r arch))
-  else ifneq ($(wildcard $(MYPLAT)),)
-    # custom platform, read the "arch" and "plat-name" fields from the toml file
-    _arch :=  $(patsubst "%",%,$(shell axconfig-gen $(MYPLAT) -r arch))
-    PLAT_NAME := $(patsubst "%",%,$(shell axconfig-gen $(MYPLAT) -r platform))
-    PLAT_PACKAGE := $(patsubst "%",%,$(shell axconfig-gen $(MYPLAT) -r package))
-    PLAT_CONFIG := $(MYPLAT)
-  else
-    builtin_platforms := $(foreach pair,$(builtin_platforms_map),$(firstword $(subst :, ,$(pair))))
-    $(error "MYPLAT" must be a valid path to a toml file)
+    PLAT_CONFIG := $(shell cargo axplat info -c $(PLAT_PACKAGE) 2>/dev/null)
   endif
+  ifeq ($(PLAT_CONFIG),)
+    $(error "MYPLAT" must be a valid configuration file path or a valid package name)
+  endif
+  # Read the architecture name from the configuration file
+  _arch :=  $(patsubst "%",%,$(shell axconfig-gen $(PLAT_CONFIG) -r arch))
   ifeq ($(origin ARCH),command line)
     ifneq ($(ARCH),$(_arch))
-      $(error "ARCH=$(ARCH)" is not compatible with "PLAT_NAME=$(PLAT_NAME)")
+      $(error "ARCH=$(ARCH)" is not compatible with "MYPLAT=$(MYPLAT)")
     endif
   endif
   ARCH := $(_arch)
 endif
+
+PLAT_NAME := $(patsubst "%",%,$(shell axconfig-gen $(PLAT_CONFIG) -r platform))
 
 default_package := axplat-x86-pc axplat-riscv64-qemu-virt axplat-aarch64-qemu-virt axplat-loongarch64-qemu-virt
 ifeq ($(filter $(PLAT_PACKAGE),$(default_package)),)
